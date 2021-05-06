4f4fa7e0df7929ee69ea0bce6ab429a4e11407b5
CtPeng-USTC-ND <pct@mail.ustc.edu.cn>
2021-05-06 15:47:08 +0800

    Update loss.py

--- RECIST_mark_propagation/loss.py
+++ RECIST_mark_propagation/loss.py
@@ -20,34 +20,33 @@ def mse_cosine_loss(y_true, y_pred):
     mse = K.variable([0.])
     cos=K.variable([0.])
     for i in range(batch_size):
-        b_c, b_r, l_c, l_r, r_c, r_r, t_c, t_r = get_point(i, y_pred)
-        pred_pts = tf.convert_to_tensor([b_c, b_r, l_c, l_r, r_c, r_r, t_c, t_r], 'float32')
-        b_c_, b_r_, l_c_, l_r_, r_c_, r_r_, t_c_, t_r_ = get_point(i, y_true)
-        gt_pts = tf.convert_to_tensor([b_c_, b_r_, l_c_, l_r_, r_c_, r_r_, t_c_, t_r_], 'float32')
+        t_r, t_c, b_r, b_c, l_r, l_c, r_r, r_c = get_point(i, y_pred)
+        pred_pts = tf.convert_to_tensor([t_r, t_c, b_r, b_c, l_r, l_c, r_r, r_c], 'float32')
+        t_r_, t_c_, b_r_, b_c_, l_r_, l_c_, r_r_, r_c_ = get_point(i, y_true)
+        gt_pts = tf.convert_to_tensor([t_r_, t_c_, b_r_, b_c_, l_r_, l_c_, r_r_, r_c_], 'float32')
         mse = mse+ mean_squared_error(gt_pts, pred_pts)
         v11 = K.cast(b_r, 'float32') - K.cast(t_r, 'float32')
         v12 = K.cast(b_c, 'float32') - K.cast(t_c, 'float32')
         v21 = K.cast(r_r, 'float32')-K.cast(l_r, 'float32')
         v22 = K.cast(r_c, 'float32')-K.cast(l_c, 'float32')
         cos = cos + K.abs((v11*v21+v12*v22)/(K.sqrt(v11*v11+v12*v12)*K.sqrt(v21*v21+v22*v22)+smooth))
-    return (mse+cos)/batch_size
-
+    return 0.05*(mse+0*cos)
 
 def get_point(i, y_pred):
     topm = y_pred[i, :, :, 0]
     bottomm = y_pred[i, :, :, 1]
     leftm = y_pred[i, :, :, 2]
     rightm = y_pred[i, :, :, 3]
-    bt_f = K.flatten(bottomm)
     tp_f = K.flatten(topm)
+    bt_f = K.flatten(bottomm)
     lt_f = K.flatten(leftm)
     rt_f = K.flatten(rightm)
-    b_r = est_argmax(bt_f) / 64
-    b_c = est_argmax(bt_f) % 64
     t_r = est_argmax(tp_f) / 64
     t_c = est_argmax(tp_f) % 64
+    b_r = est_argmax(bt_f) / 64
+    b_c = est_argmax(bt_f) % 64
     l_r = est_argmax(lt_f) / 64
     l_c = est_argmax(lt_f) % 64
     r_r = est_argmax(rt_f) / 64
     r_c = est_argmax(rt_f) % 64
-    return b_c, b_r, l_c, l_r, r_c, r_r, t_c, t_r
+    return t_r, t_c, b_r, b_c, l_r, l_c, r_r, r_c
